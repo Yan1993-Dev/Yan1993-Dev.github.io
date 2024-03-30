70eaf68c0faf21e2534c3633c6fd1a2f18898e0c
Yan <taur.game@gmail.com>
2024-03-30 22:49:27 +0400

    321

--- script.js
+++ script.js
@@ -1,105 +1,208 @@
-// const popup = document.querySelector('.popup');
-// const openBtn = document.querySelector('.div1');
-// const popup = document.querySelector('.popup');
-// const openBtn = document.querySelector('.div1');
-// const popup = document.querySelector('.popup');
-// const openBtn = document.querySelector('.div1');
-// const popup = document.querySelector('.popup');
-// const openBtn = document.querySelector('.div1');
+(function () {
+    const popup1 = document.querySelector('.popup1');
+    const openBtn1 = document.querySelector('.div1');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn1.addEventListener("click", function () {
+        popup1.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup1.addEventListener("click", function () {
+        popup1.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup2 = document.querySelector('.popup2');
+    const openBtn2 = document.querySelector('.div2');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn2.addEventListener("click", function () {
+        popup2.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup2.addEventListener("click", function () {
+        popup2.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup3 = document.querySelector('.popup3');
+    const openBtn3 = document.querySelector('.div3');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn3.addEventListener("click", function () {
+        popup3.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup3.addEventListener("click", function () {
+        popup3.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup4 = document.querySelector('.popup4');
+    const openBtn4 = document.querySelector('.div4');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn4.addEventListener("click", function () {
+        popup4.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup4.addEventListener("click", function () {
+        popup4.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup5 = document.querySelector('.popup5');
+    const openBtn5 = document.querySelector('.div5');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn5.addEventListener("click", function () {
+        popup5.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup5.addEventListener("click", function () {
+        popup5.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup6 = document.querySelector('.popup6');
+    const openBtn6 = document.querySelector('.div6');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn6.addEventListener("click", function () {
+        popup6.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup6.addEventListener("click", function () {
+        popup6.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup7 = document.querySelector('.popup7');
+    const openBtn7 = document.querySelector('.div7');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn7.addEventListener("click", function () {
+        popup7.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup7.addEventListener("click", function () {
+        popup7.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup8 = document.querySelector('.popup8');
+    const openBtn8 = document.querySelector('.div8');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn8.addEventListener("click", function () {
+        popup8.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup8.addEventListener("click", function () {
+        popup8.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup9 = document.querySelector('.popup9');
+    const openBtn9 = document.querySelector('.div9');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn9.addEventListener("click", function () {
+        popup9.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup9.addEventListener("click", function () {
+        popup9.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
+
+(function () {
+    const popup10 = document.querySelector('.popup10');
+    const openBtn10 = document.querySelector('.div10');
+    const overlay = document.querySelector('.overlay');
+
+    openBtn10.addEventListener("click", function () {
+        popup10.classList.add("open")
+        overlay.classList.add("open")
+    })
+
+    popup10.addEventListener("click", function () {
+        popup10.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
 
 
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
+(function () {
+    const popup11 = document.querySelector('.popup11');
+    const openBtn11 = document.querySelector('.div11');
+    const overlay = document.querySelector('.overlay');
 
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
+    openBtn11.addEventListener("click", function () {
+        popup11.classList.add("open")
+        overlay.classList.add("open")
+    })
 
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
+    popup11.addEventListener("click", function () {
+        popup11.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
 
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
+(function () {
+    const popup12 = document.querySelector('.popup12');
+    const openBtn12 = document.querySelector('.div12');
+    const overlay = document.querySelector('.overlay');
 
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
+    openBtn12.addEventListener("click", function () {
+        popup12.classList.add("open")
+        overlay.classList.add("open")
+    })
 
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
+    popup12.addEventListener("click", function () {
+        popup12.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
 
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
+(function () {
+    const popup13 = document.querySelector('.popup13');
+    const openBtn13 = document.querySelector('.div13');
+    const overlay = document.querySelector('.overlay');
 
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
+    openBtn13.addEventListener("click", function () {
+        popup13.classList.add("open")
+        overlay.classList.add("open")
+    })
 
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
-
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
-
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
-
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
-
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
-
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
-
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
-
-// openBtn.addEventListener("click", function () {
-//     popup.classList.add("open")
-// })
-
-// popup.addEventListener("click", function () {
-//     popup.classList.remove("open")
-// })
+    popup13.addEventListener("click", function () {
+        popup13.classList.remove("open")
+        overlay.classList.remove("open")
+    })
+})();
